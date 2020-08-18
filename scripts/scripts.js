76efd21da06d2ebd881a5d13dd6f48e2fa5e031e
avishkakavindu <avishkakavindud@gmail.com>
2020-08-18 17:23:40 +0530

    js coockie handling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,21 +7,37 @@ for(i=0; i<updateBtns.length; i++) {
         var action = this.dataset.action
         //console.log('productId: ', productId, ' | Action: ', action)
         
-        addCookieItem(productId, action)
+        updateUserOrder(productId, action);
+        addCookieItem(productId, action);
     })
 }
 
-function addCookieItem(productId, action) {
-    if(action == 'add') {
-        if(cart[productId] == undefined) {
-            cart[productId] = {'quantity': 1}
-        }else {
-            cart[productId]['quantity'] += 1
-        }
-        console.log('addCookieItem: ', cart)
-    }
-    if(action == 'remove') {
-        console.log('addCookieItem- Action:REMOVE |ID: ', productId)
-    }
-    
+// Update current user order
+function updateUserOrder() {
+    console.log("Update user order request detected!");
 }
+
+// function addCookieItem(productId, action) {
+//     console.log("XXXX: ", myCart);
+//     if(action == 'add') {
+//         if(myCart[productId] == undefined) {
+//             myCart[productId] = {'quantity': 1};
+
+//         }else {
+//             myCart[productId]['quantity'] += 1;
+//         }
+//         console.log('addCookieItem: ', myCart);
+
+//         console.log("---", myCart);
+//     }
+//     if(action == 'remove') {
+//         myCart[productId]['quantity'] -= 1;
+
+//         if(myCart[productId]['quantity'] <= 0) {
+//             console.log('Item should be deleted!!!');
+//             delete myCart[productId];
+//         } 
+//     }
+//     console.log('My Cart:', myCart);
+//     document.cookie = 'myCart=' + JSON.stringify(myCart) + ";domain=;path=/";
+// }
