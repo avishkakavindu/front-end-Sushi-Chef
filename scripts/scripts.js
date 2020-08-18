2b955f3de1deedac59d37c02d9f733e9684b8efa
avishkakavindu <avishkakavindud@gmail.com>
2020-08-18 13:56:40 +0530

    js coockie handling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,27 @@
 var updateBtns = document.getElementsByClassName('update-cart');
 
 // update cart items (quantity, newitems) 
-for(i=0; i<updateBtns.length; i++){
-    updateBtns[i].addEventListener('click', function(){
+for(i=0; i<updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function() {
         var productId = this.dataset.product
         var action = this.dataset.action
-        console.log('productId: ', productId, ' | Action: ', action)
+        //console.log('productId: ', productId, ' | Action: ', action)
+        
+        addCookieItem(productId, action)
     })
+}
+
+function addCookieItem(productId, action) {
+    if(action == 'add') {
+        if(cart[productId] == undefined) {
+            cart[productId] = {'quantity': 1}
+        }else {
+            cart[productId]['quantity'] += 1
+        }
+        console.log('addCookieItem: ', cart)
+    }
+    if(action == 'remove') {
+        console.log('addCookieItem- Action:REMOVE |ID: ', productId)
+    }
+    
 }
